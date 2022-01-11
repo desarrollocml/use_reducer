569e9ae9f2292499094f8514a60d00e766588dfa
desarrollocml <cristianmirandalamas@gmail.com>
2022-01-10 22:32:02 -0300

    con switch

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -3,27 +3,33 @@ import { useReducer } from "react";
 const initialState = 0;
 //const reducer = (state, action) => {
 const reducer = (state, action) => {
-  if (action.type === "increment") {
-    return state + 1;
-  }
-  if (action.type === "decrement") {
-    return state - 1;
-  }
-  if (action.type === "reset") {
-    return initialState;
-  }
-  return state;
+  switch(action.type){
+      case "increment":
+          return state+1;
+      case "decrement":
+          return state-1;
+      case "reset":
+          return 0;
+      default :
+        return state;    
+
+  }  
+
 };
 
 export default function CounterApp() {
   //const [state, setstate] = useState(initialState)
   //const [state, dispatch] = useReducer(reducer, initialState, init);
- 
+
   const [counter, dispatch] = useReducer(reducer, initialState);
   return (
     <div>
       <h1>Clicks: {counter}</h1>
-      <button onClick={() => dispatch({ type: "increment" })}>Increment</button>
+      <button
+        onClick={() => dispatch({ type: "increment"})}
+      >
+        Increment
+      </button>
       <button onClick={() => dispatch({ type: "decrement" })}>Decrement</button>
       <button onClick={() => dispatch({ type: "reset" })}>Reset</button>
     </div>
